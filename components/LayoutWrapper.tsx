4b7e9e759d979ba2d1bdef203b0f0afa2d7205ac
Filali-anass <anass.filali.93@gmail.com>
2022-08-19 01:30:28 +0100

    turn it to blog using timlrx/tailwind-nextjs-starter-blog

---
+++ components/LayoutWrapper.tsx
@@ -0,0 +1,64 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.png'
+import WhiteLogo from '@/data/logo-white.png'
+import Link from './Link'
+import SectionContainer from './SectionContainer'
+import Footer from './Footer'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+import { ReactNode } from 'react'
+import Image from 'next/image'
+
+interface Props {
+  children: ReactNode
+}
+
+const LayoutWrapper = ({ children }: Props) => {
+  return (
+    <SectionContainer>
+      <div className="flex h-screen flex-col justify-between">
+        <header className="flex items-center justify-between py-10">
+          <div>
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div className="h-0 w-0 dark:mr-3 dark:h-32 dark:w-32 ">
+                  <Image src={WhiteLogo} alt="Logo" />
+                </div>
+                <div className="mr-3 h-32 w-32  dark:mr-0 dark:h-0 dark:w-0 ">
+                  <Image src={Logo} alt="Logo" />
+                </div>
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                    {siteMetadata.headerTitle}
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </header>
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </div>
+    </SectionContainer>
+  )
+}
+
+export default LayoutWrapper
